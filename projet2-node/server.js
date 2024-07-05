92d4e2bc0cdebf6fa311ea55cc4c4611bdc7a7b2
Junior MEME <andermonmeme@gmail.com>
2024-07-05 16:50:05 +0100

    Ajout de index.html

--- projet2-node/server.js
+++ projet2-node/server.js
@@ -5,8 +5,8 @@ const app = express();
 const port = 5000;
 
 // Route pour la page d'accueil
-app.get('/home', (req, res) => {
-  res.send('Bienvenue sur la page d\'accueil !');
+app.get('/', (req, res) => {
+  res.sendFile('/home/ec2-user/notre-site/index.html')
   console.log('Tout fonctionne normalement');
 });
 
@@ -19,6 +19,7 @@ app.get('/help', (req, res) => {
 // Route par défaut (tout le reste)
 app.get('*', (req, res) => {
   res.status(404).send('Désolé, cette page n\'existe pas.');
+  console.error('Rrreur 404 du site node js 1 port 5000 ');
 });
 
 app.listen(port, () => {
